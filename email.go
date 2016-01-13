cda1ab88591401d1dd3c2fbb643ef59a047baa10
Cathal Garvey <cathalgarvey@cathalgarvey.me>
2016-01-13 16:59:04 +0000

    Recipient list parsing now works!

--- email.go
+++ email.go
@@ -9,6 +9,9 @@ import (
 	"github.com/jordan-wright/email"
 )
 
+// ErrEmailUnparseable - Returned when a To/CC/BCC entry can't be parsed into a simple Email address.
+var ErrEmailUnparseable = errors.New("Email appears neither a simple ('foo@bar.com') nor expressive ('Foo Bar <foo@bar.com>') construction")
+
 // Email is a derivation of email.Email with a few methods added to play nicely
 // in lua.
 type Email struct {
@@ -25,6 +28,26 @@ func WrapEmail(e *email.Email) *Email {
 	newe.Email = e
 	newe.inRecipientLists = make(map[string]struct{})
 	return newe
+}
+
+// GetHeader is a direct call to email.Headers.Get
+func (em *Email) GetHeader(key string) string {
+	return em.Headers.Get(key)
+}
+
+// AddHeader is a direct call to email.Headers.Add
+func (em *Email) AddHeader(key, value string) {
+	em.Headers.Add(key, value)
+}
+
+// DelHeader is a direct call to email.Headers.Del
+func (em *Email) DelHeader(key string) {
+	em.Headers.Del(key)
+}
+
+// SetHeader is a direct call to email.Headers.Set
+func (em *Email) SetHeader(key, value string) {
+	em.Headers.Set(key, value)
 }
 
 // Check recipient roster
@@ -121,91 +144,59 @@ func (em *Email) RemoveRecipient(email string) {
 	em.remRecipient(email)
 }
 
+// This patches over issues with the `email` package where sometimes the "To"
+// header is a single string as the first entry in the "To" field of the Email
+// struct. It also DRYs out the NormaliseRecipients function. To help the Logger,
+// this function accepts a string arg naming the field under iteration.
+// This adds all seen emails to the Email.inRecipientLists set.
+func (em *Email) normaliseEmailSlice(field string, emailSlice []string) []string {
+	if len(emailSlice) == 0 {
+		return nil
+	}
+	newField := make([]string, 0, len(emailSlice))
+	for _, entry := range emailSlice {
+		// First, split multi-entry bits if necessary.. Look for ">" chars that don't
+		// end the line, and try to extract emails from each such substring using
+		// parseExpressiveEmail()
+		multiEntries := parseMultiExpressiveEmails(entry)
+		for _, e := range multiEntries {
+			log.Println("Range over '" + field + "' entry: " + e)
+			e, err := parseExpressiveEmail(e)
+			if err != nil {
+				log.Println("Error parsing address from '" + field + "' email recipient: " + e)
+				continue
+			}
+			if _, ok := em.inRecipientLists[e]; ok {
+				log.Println("Skipping recipient as it's already been seen: " + e)
+				continue
+			} else {
+				em.inRecipientLists[e] = struct{}{}
+			}
+			log.Println("Appending address to Normalised '" + field + "' list: " + e)
+			newField = append(newField, e)
+		}
+	}
+	return newField
+}
+
 // NormaliseRecipients runs through the To, CC, and BCC lists and normalises all
 // emails contained therein, as well as deduplicating emails in that order.
 // This is run prior to calling eventLoop in Lua, and added emails are all
 // normalised under the hood, so there should be no need to call this from Lua.
 func (em *Email) NormaliseRecipients() {
-	// TODO: If-gate all the below to only run if the lists are of nonzero length.
-	newTo := make([]string, 0, len(em.To))
-	for _, e := range em.To {
-		e, err := parseExpressiveEmail(e)
-		if err != nil {
-			log.Println("Error parsing address from 'To' email recipient: " + e)
-			continue
-		}
-		if _, ok := em.inRecipientLists[e]; ok {
-			log.Println("Skipping recipient as it's already been seen: " + e)
-			continue
-		} else {
-			em.inRecipientLists[e] = struct{}{}
-		}
-		log.Println("Appending address to Normalised 'To' list: " + e)
-		newTo = append(newTo, e)
+	newTo := em.normaliseEmailSlice("To", em.To)
+	if newTo != nil {
+		em.To = append(em.To[:0], newTo...)
 	}
-	// Overwrites?
-	em.To = append(em.To[:0], newTo...)
-
-	newCc := make([]string, 0, len(em.Cc))
-	for _, e := range em.Cc {
-		e, err := parseExpressiveEmail(e)
-		if err != nil {
-			log.Println("Error parsing address from 'CC' email recipient: " + e)
-			continue
-		}
-		if _, ok := em.inRecipientLists[e]; ok {
-			log.Println("Skipping recipient as it's already been seen: " + e)
-			continue
-		} else {
-			em.inRecipientLists[e] = struct{}{}
-		}
-		log.Println("Appending address to Normalised 'CC' list: " + e)
-		newCc = append(newCc, e)
+	newCc := em.normaliseEmailSlice("Cc", em.Cc)
+	if newCc != nil {
+		em.Cc = append(em.Cc[:0], newCc...)
 	}
-	em.Cc = append(em.Cc[:0], newCc...)
-
-	// Mostly this won't even run? Incoming mails should have blank Bcc lists.
-	newBcc := make([]string, 0, len(em.Bcc))
-	for _, e := range em.Bcc {
-		e, err := parseExpressiveEmail(e)
-		if err != nil {
-			log.Println("Error parsing address from 'BCC' email recipient: " + e)
-			continue
-		}
-		if _, ok := em.inRecipientLists[e]; ok {
-			log.Println("Skipping recipient as it's already been seen: " + e)
-			continue
-		} else {
-			em.inRecipientLists[e] = struct{}{}
-		}
-		log.Println("Appending address to Normalised 'Bcc' list: " + e)
-		newBcc = append(newBcc, e)
+	newBcc := em.normaliseEmailSlice("Bcc", em.Bcc)
+	if newBcc != nil {
+		em.Bcc = append(em.Bcc[:0], newBcc...)
 	}
-	em.Bcc = append(em.Bcc[:0], newBcc...)
 }
-
-// GetHeader is a direct call to email.Headers.Get
-func (em *Email) GetHeader(key string) string {
-	return em.Headers.Get(key)
-}
-
-// AddHeader is a direct call to email.Headers.Add
-func (em *Email) AddHeader(key, value string) {
-	em.Headers.Add(key, value)
-}
-
-// DelHeader is a direct call to email.Headers.Del
-func (em *Email) DelHeader(key string) {
-	em.Headers.Del(key)
-}
-
-// SetHeader is a direct call to email.Headers.Set
-func (em *Email) SetHeader(key, value string) {
-	em.Headers.Set(key, value)
-}
-
-// ErrEmailUnparseable - Returned when a To/CC/BCC entry can't be parsed into a simple Email address.
-var ErrEmailUnparseable = errors.New("Email appears neither a simple ('foo@bar.com') nor expressive ('Foo Bar <foo@bar.com>') construction")
 
 // parseExpressiveEmail - Given a line "Foo Bar <foo@bar.com>", return "foo@bar.com".
 // For "foo@bar.com" return simply that!
@@ -230,4 +221,39 @@ func parseExpressiveEmail(emailLine string) (string, error) {
 		return emailLine, ErrEmailUnparseable
 	}
 	return normed, nil
+}
+
+// Given a string like "Cathal Garvey <cathal@foo.com>, Stephen Barr <steve@foo.com>"
+// return []string{"Cathal Garvey <cathal@foo.com>", "Stephen Barr <steve@foo.com>"}
+func parseMultiExpressiveEmails(entry string) []string {
+	log.Println("parseMultiExpressiveEmails called on " + entry)
+	// Shortcuts
+	if validator.IsValidEmail(entry) {
+		return []string{entry}
+	}
+	if !(strings.Contains(entry, ">") && strings.Contains(entry, ",")) {
+		return []string{entry}
+	}
+	var entries []string
+	// Find each comma-after-closebracket and slice around it.
+	for i := 0; i < len(entry); {
+		nextBracket := strings.Index(entry[i:], ">")
+		if nextBracket == -1 {
+			entries = append(entries, entry[i:])
+			break
+		}
+		// nextComma gets the index *after the bracket* so needs to be added to
+		// the nextBracket index!
+		distCommaAfterBracket := strings.Index(entry[nextBracket:], ",")
+		if distCommaAfterBracket == -1 {
+			entries = append(entries, entry[i:])
+			break
+		}
+		nextComma := distCommaAfterBracket + nextBracket
+		chunk := entry[i:nextComma]
+		entries = append(entries, chunk)
+		i = nextComma + 1
+	}
+	log.Println("parseMultiExpressiveEmails returning: " + strings.Join(entries, " | "))
+	return entries
 }
