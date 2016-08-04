0b8b56dbfe5d1477cadbab2aea729609e9f32de9
Cathal Garvey <cathalgarvey@cathalgarvey.me>
2016-08-04 15:42:16 +0100

    Refactored the 'sub' command set entirely to make it more consistent

--- main.go
+++ main.go
@@ -21,35 +21,136 @@ var (
 	execConfigfile = execMode.Arg("configfile", "Location of config file.").Required().String()
 	execScript     = execMode.Arg("script", "Location of lua script to execute.").Required().String()
 
-	subMode       = app.Command("sub", "Add / Remove subscribers to a list.")
-	subConfigfile = subMode.Arg("configfile", "Location of config file.").Required().String()
-	subAction     = subMode.Arg("action", "Member edit command to use: list | add | update | remove").Required().Enum("add", "update", "remove", "list")
-	subAddMod     = subMode.Flag("moderator", "Make new user a moderator").Default("false").Bool()
-	subAddPost    = subMode.Flag("can-post", "Allow new user to post").Default("true").Bool()
-	subEmail      = subMode.Arg("email", "Email address of new/edited/removed user").String()
-	subName       = subMode.Arg("name", "Name of new/edited user").String()
+	subMode = app.Command("sub", "Without another command, print subscriber list")
+
+	subListMode    = subMode.Command("list", "List subscribers")
+	subLConfigFile = subListMode.Arg("configfile", "Location of config file.").Required().String()
+
+	subUpdateAction = subMode.Command("update", "Add or edit a subscriber")
+	subUConfigFile  = subUpdateAction.Arg("configfile", "Location of config file").Required().String()
+	subUEmail       = subUpdateAction.Flag("email", "Email address to add or update details for").Required().String()
+	subUName        = subUpdateAction.Flag("name", "Name of subscriber to add or update details for. Required when adding.").String()
+	subUMod         = subUpdateAction.Flag("moderator", "Mark the new/updated user as a moderator").Bool()
+	subUPost        = subUpdateAction.Flag("can-post", "Indicate that the new/updated user may post to the list").Bool()
+
+	subRemoveAction = subMode.Command("remove", "Remove a subscriber")
+	subRConfigFile  = subRemoveAction.Arg("configfile", "Location of config file").Required().String()
+	subREmail       = subRemoveAction.Flag("email", "Email address of user to remove").Required().String()
 )
 
 func main() {
 	log15.Info("Welcome to Listless!", log15.Ctx{"context": "setup"})
-	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
+	cmd := kingpin.MustParse(app.Parse(os.Args[1:]))
+	switch cmd {
 	case loopMode.FullCommand():
-		{
-			loopModeF()
-		}
+		loopModeF()
 	case execMode.FullCommand():
+		execModeF()
+	case subUpdateAction.FullCommand():
+		subUpdateModeF()
+	case subRemoveAction.FullCommand():
+		subRemoveModeF()
+	case subListMode.FullCommand():
+		subListModeF()
+	default:
+		log.Fatal("No valid command given. Try '--help' for ideas.")
+	}
+}
+
+func subUpdateModeF() {
+	log15.Info("Starting in subscriber mode", log15.Ctx{"context": "setup"})
+	config := loadSettings(*subUConfigFile)
+	log15.Info("Loading Engine", log15.Ctx{"context": "setup"})
+	engine, err := NewEngine(config)
+	if err != nil {
+		log15.Error("Failed to load Engine", log15.Ctx{"context": "setup", "error": err})
+		log.Fatal(err)
+	}
+	email := normaliseEmail(*subUEmail)
+	if email == "" {
+		panic("Provided email address failed to normalise: " + *subUEmail)
+	}
+	// Does user exist, or is user being added?
+	usrmeta, err := engine.DB.GetSubscriber(email)
+	switch err {
+	case nil:
 		{
-			execModeF()
+			// Edit mode
+			if usrmeta == nil {
+				panic("usrmeta is unexpectedly nil after getting a nil error from DB.GetSubscriber")
+			}
+			if subUName != nil && *subUName != "" {
+				usrmeta.Name = *subUName
+			}
+			if subUMod != nil {
+				usrmeta.Moderator = *subUMod
+			}
+			if subUPost != nil {
+				usrmeta.AllowedPost = *subUPost
+			}
+			engine.DB.UpdateSubscriber(email, usrmeta)
 		}
-	case subMode.FullCommand():
+	case ErrMemberEntryNotFound:
 		{
-			subModeF()
+			// Add mode
+			name := ""
+			isMod := false
+			canPost := true
+			if subUName == nil {
+				panic("Require a name when adding a new member")
+			} else {
+				name = *subUName
+			}
+			if subUMod != nil {
+				isMod = *subUMod
+			}
+			if subUPost != nil {
+				canPost = *subUPost
+			}
+			usrmeta := engine.DB.CreateSubscriber(email, name, canPost, isMod)
+			engine.DB.UpdateSubscriber(email, usrmeta)
 		}
 	default:
 		{
-			log.Fatal("No valid command given. Try '--help' for ideas.")
+			panic(err)
 		}
 	}
+}
+
+func subRemoveModeF() {
+	// Indempotent for simplicity.
+	log15.Info("Starting in subscriber mode", log15.Ctx{"context": "setup"})
+	config := loadSettings(*subRConfigFile)
+	log15.Info("Loading Engine", log15.Ctx{"context": "setup"})
+	engine, err := NewEngine(config)
+	if err != nil {
+		log15.Error("Failed to load Engine", log15.Ctx{"context": "setup", "error": err})
+		log.Fatal(err)
+	}
+	email := normaliseEmail(*subREmail)
+	if email == "" {
+		panic("Provided email address failed to normalise: " + *subREmail)
+	}
+	err = engine.DB.DelSubscriber(email)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func subListModeF() {
+	log15.Info("Starting in subscriber mode", log15.Ctx{"context": "setup"})
+	config := loadSettings(*subLConfigFile)
+	log15.Info("Loading Engine", log15.Ctx{"context": "setup"})
+	engine, err := NewEngine(config)
+	if err != nil {
+		log15.Error("Failed to load Engine", log15.Ctx{"context": "setup", "error": err})
+		log.Fatal(err)
+	}
+	fmt.Println("Email,Name,Moderator,AllowedPost")
+	engine.DB.forEachSubscriber(func(email string, meta *MemberMeta) error {
+		fmt.Printf("%s,%s,%v,%v\n", email, meta.Name, meta.Moderator, meta.AllowedPost)
+		return nil
+	})
 }
 
 func loopModeF() {
@@ -89,72 +190,6 @@ func execModeF() {
 	if err != nil {
 		log15.Error("Failed to execute script", log15.Ctx{"context": "setup", "error": err, "script": *execScript})
 		log.Fatal(err)
-	}
-}
-
-func subModeF() {
-	log15.Info("Starting in subscriber mode", log15.Ctx{"context": "setup"})
-	config := loadSettings(*subConfigfile)
-	log15.Info("Loading Engine", log15.Ctx{"context": "setup"})
-	engine, err := NewEngine(config)
-	if err != nil {
-		log15.Error("Failed to load Engine", log15.Ctx{"context": "setup", "error": err})
-		log.Fatal(err)
-	}
-	switch *subAction {
-	case "add":
-		{
-			if subEmail == nil || subName == nil || *subEmail == "" || *subName == "" {
-				panic("Missing email or name argument for new subscriber.")
-			}
-			log15.Info("Adding user to subscriber", log15.Ctx{"context": "setup", "subEmail": *subEmail, "subName": *subName})
-			meta := engine.DB.CreateSubscriber(*subEmail, *subName, *subAddPost, *subAddMod)
-			err = engine.DB.UpdateSubscriber(*subEmail, meta)
-			if err != nil {
-				panic(err)
-			}
-		}
-	case "update":
-		{
-			if subEmail == nil {
-				panic("Missing email argument to update subscriber")
-			}
-			meta, err := engine.DB.GetSubscriber(*subEmail)
-			if err != nil {
-				panic(err)
-			}
-			if subName != nil {
-				meta.Name = *subName
-			}
-			if subAddPost != nil {
-				meta.AllowedPost = *subAddPost
-			}
-			if subAddMod != nil {
-				meta.Moderator = *subAddMod
-			}
-			err = engine.DB.UpdateSubscriber(*subEmail, meta)
-			if err != nil {
-				panic(err)
-			}
-		}
-	case "remove":
-		{
-			if subEmail == nil {
-				panic("Missing email argument to remove subscriber")
-			}
-			err = engine.DB.DelSubscriber(*subEmail)
-			if err != nil {
-				panic(err)
-			}
-		}
-	case "list":
-		{
-			fmt.Println("Email,Name,Moderator,AllowedPost")
-			engine.DB.forEachSubscriber(func(email string, meta *MemberMeta) error {
-				fmt.Printf("%s,%s,%v,%v\n", email, meta.Name, meta.Moderator, meta.AllowedPost)
-				return nil
-			})
-		}
 	}
 }
 
